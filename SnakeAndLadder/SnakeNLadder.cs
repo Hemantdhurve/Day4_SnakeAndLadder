8bf7e2af948f5b5de7308ead2fd17381db8d42ee
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-16 01:51:04 +0530

    Added UC3 Player position to check options

--- SnakeAndLadder/SnakeNLadder.cs
+++ SnakeAndLadder/SnakeNLadder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,17 @@ namespace SnakeAndLadder
 {
     public class SnakeNLadder
     {
+        //Constants
+
+        public const int Ladder = 1;
+        public const int Snake = 2;
+
         public void SnakeLadder()
         {
+            //variables
             int position1 = 0;
             int dieRoll=0;
+
             Console.WriteLine("Initial Position of Player 1 is :" + position1 );
 
             //Random function to roll die 1 to 6
@@ -21,7 +29,41 @@ namespace SnakeAndLadder
             Console.WriteLine("Die Roll :" + dieRoll);
             position1=dieRoll;
             Console.WriteLine("Position1 of player after dieRoll is :" + position1);
-            Console.ReadLine();
+            
+            int options=random.Next(0,3);
+            Console.WriteLine("After getting Random Option is :" + options);
+
+            //using switch case to check options
+
+            switch (options)
+            {
+                case Ladder:
+                    Console.WriteLine("In option we getting Ladder");
+                    position1+=dieRoll;
+                    Console.WriteLine("New position1 after getting Ladder :" + position1);
+                    break;
+
+                case Snake:
+                    Console.WriteLine("In option player bit by snake");
+                    position1-=dieRoll;
+                    Console.WriteLine("\"New position1 after snake bit :" + position1);
+                    
+                    //check if position is zero after snake bit then given position zero
+                    
+                    if(position1 <0)
+                        position1 = 0;
+                    Console.WriteLine("Player position1 after snake bit need to start from initial position : " + position1);
+                    break ;
+
+                default:
+                    Console.WriteLine("In option we got No play :" + position1);
+                   
+                    break;
+
+
+            }
+
+
 
         }
     }
