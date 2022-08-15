16e8384924db69418f42effc736919ca4eec2177
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-16 00:38:49 +0530

    Added UC1 Position of Player 1

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,6 +7,11 @@ namespace SnakeAndLadder
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Problem");
+
+            //method is non-static so we need to instantiate method
+            SnakeNLadder obj=new SnakeNLadder();
+            obj.SnakeLadder();
+
         }
     }
 }
---
+++ SnakeAndLadder/SnakeNLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeNLadder
+    {
+        public void SnakeLadder()
+        {
+            int position1 = 0;
+            Console.WriteLine("Initial Position of Player 1 is :" + position1 );
+        }
+    }
+
+}
