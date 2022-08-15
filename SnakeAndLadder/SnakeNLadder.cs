56cd4deb3f00894469333f16aeaaf05b27235c3e
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-16 00:54:31 +0530

    Added UC2 Die Roll Randomly

--- SnakeAndLadder/SnakeNLadder.cs
+++ SnakeAndLadder/SnakeNLadder.cs
@@ -11,7 +11,18 @@ namespace SnakeAndLadder
         public void SnakeLadder()
         {
             int position1 = 0;
+            int dieRoll=0;
             Console.WriteLine("Initial Position of Player 1 is :" + position1 );
+
+            //Random function to roll die 1 to 6
+            Random random = new Random();
+            dieRoll = random.Next(1,7);                 //range taken from 1 to 7 due to die have 1 to 6 numbers
+
+            Console.WriteLine("Die Roll :" + dieRoll);
+            position1=dieRoll;
+            Console.WriteLine("Position1 of player after dieRoll is :" + position1);
+            Console.ReadLine();
+
         }
     }
 
